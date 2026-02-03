c2c1f6cc7ecb02fc33f5e687f510fe2c1121b732
Clawwo <genzz760@gmail.com>
2026-02-03 09:35:54 +0700

    fix: memperbaiki ecosystemn file

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,11 +4,12 @@ module.exports = {
       name: 'nest-api',
       script: 'dist/main.js',
       env: {
-        PGSSLROOTCERT: '/home/iasproductama/supabase-root-2021.pem',
+        PGSSLROOTCERT: '/home/iasproductama/supabase-ca-bundle.pem',
+        NODE_EXTRA_CA_CERTS: '/home/iasproductama/supabase-ca-bundle.pem',
         DATABASE_URL:
-          'postgresql://postgres.fysjbxgdbjqhalesmlri:DKIkFRZIKBuZbtPU@aws-1-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require&sslrootcert=/home/iasproductama/supabase-root-2021.pem',
+          'postgresql://postgres.fysjbxgdbjqhalesmlri:DKIkFRZIKBuZbtPU@aws-1-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require&sslrootcert=/home/iasproductama/supabase-ca-bundle.pem',
         DIRECT_URL:
-          'postgresql://postgres.fysjbxgdbjqhalesmlri:DKIkFRZIKBuZbtPU@aws-1-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require&sslrootcert=/home/iasproductama/supabase-root-2021.pem',
+          'postgresql://postgres.fysjbxgdbjqhalesmlri:DKIkFRZIKBuZbtPU@aws-1-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require&sslrootcert=/home/iasproductama/supabase-ca-bundle.pem',
         CORS_ORIGINS:
           'http://localhost:5173,http://127.0.0.1:5173,https://iasproductama.site,https://www.iasproductama.site,https://api.iasproductama.site',
       },
